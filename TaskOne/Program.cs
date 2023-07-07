a88788fa131b3f41e7aa97c8e7e92222bea56029
Daniil <daniilkukotsky@gmail.com>
2023-07-07 13:03:08 +0400

    Commit bug

---
+++ TaskOne/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter your first number: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your second number: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (Num1 < Num2);
+
+{
+    max = Num2;
+    Console.WriteLine("Большее число: " + max);
+}
+
+else
+{
+   Console.WriteLine("Большее число: " + Num1);
+}
