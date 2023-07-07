23d31d24a7edcc280831039f26a6c20b9a50ed5a
Daniil <daniilkukotsky@gmail.com>
2023-07-07 13:21:11 +0400

    Final commit

--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,17 +1,21 @@
 ﻿Console.Write("Enter your first number: ");
 int Num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter your second number: ");
+Console.Write("Enter your second number: ");
 int Num2 = Convert.ToInt32(Console.ReadLine());
 int max = 0;
 
-if (Num1 < Num2);
+if (Num1 < Num2)
 
 {
     max = Num2;
     Console.WriteLine("Большее число: " + max);
 }
-
+else if (Num1 == Num2)
+{
+    Console.Write("Числа равны.");
+}
 else
 {
+    max = Num1;
    Console.WriteLine("Большее число: " + Num1);
 }
