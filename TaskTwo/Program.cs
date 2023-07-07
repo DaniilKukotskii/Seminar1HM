3504aa8203e6c9a75ecda709313fb5007efcfab6
Daniil <daniilkukotsky@gmail.com>
2023-07-07 14:16:52 +0400

    Final commit repository

---
+++ TaskTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Input your number: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your number: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your number: ");
+int Num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (Num2 > Num1)
+{
+    max = Num2;
+}
+if (Num3 > max)
+{
+    max = Num3;
+}
+Console.WriteLine("Самое большое число: " + max);
