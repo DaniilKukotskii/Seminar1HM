37ee3319f6a1778d6df818791a7e794853db3770
Daniil <daniilkukotsky@gmail.com>
2023-07-07 14:25:28 +0400

    Final commit repository

---
+++ TaskThree/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input your number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number %2 == 0)
+{
+    Console.Write("Число четное.");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
